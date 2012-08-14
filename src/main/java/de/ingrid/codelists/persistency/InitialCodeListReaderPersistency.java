c451529dfb131740439466f84b6ab77186c62698
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2012-08-14 13:07:18 +0000

    codelist-service: - read initial codelists as UTF-8 from file so umlaute are correct on windows !!!

--- src/main/java/de/ingrid/codelists/persistency/InitialCodeListReaderPersistency.java
+++ src/main/java/de/ingrid/codelists/persistency/InitialCodeListReaderPersistency.java
@@ -1,14 +1,20 @@
 package de.ingrid.codelists.persistency;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.StreamException;
 
 import de.ingrid.codelists.model.CodeList;
 
 public class InitialCodeListReaderPersistency implements ICodeListPersistency {
+
+	private static Log log = LogFactory.getLog(InitialCodeListReaderPersistency.class);
 
     public static String pathToXml = "codelists_initial.xml";
     
@@ -20,8 +26,10 @@ public class InitialCodeListReaderPersistency implements ICodeListPersistency {
         XStream xStream = new XStream();
         try {
             InputStream input = this.getClass().getResourceAsStream("/codelists_initial.xml");
-            return (List<CodeList>) xStream.fromXML(input);
-        } catch (StreamException e) {
+            Reader reader = new InputStreamReader(input, "UTF-8");
+            return (List<CodeList>) xStream.fromXML(reader);
+        } catch (Exception e) {
+        	log.warn("Problems reading initial codelists from file codelists_initial.xml.", e);
             //return new ArrayList<CodeList>();
         }
         return null;
